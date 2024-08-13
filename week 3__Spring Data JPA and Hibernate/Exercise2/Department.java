e8760fb18e81cf5ff91f1d446c2311b774807195
Adyasa Sahoo <94691658+adyasa-sahoo@users.noreply.github.com>
2024-08-13 21:50:56 +0530

    Add files via upload

---
+++ week 3__Spring Data JPA and Hibernate/Exercise2/Department.java
@@ -0,0 +1,25 @@
+package com.example.employeemanagementsystem.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employees;
+}
